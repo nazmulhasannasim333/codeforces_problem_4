a5f54d2004c0e3d588397ff0b004243a2cac501d
Nasim <mdnasimhosen333@gmail.com>
2025-04-24 21:55:12 +0600

    solved i love string

--- K_I_Love_strings.c
+++ K_I_Love_strings.c
@@ -4,8 +4,35 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    char s[51], t[51];
-    char res[101];
-    
+
+    for (int tc = 0; tc < n; tc++)
+    {
+        char s[51], t[51];
+        scanf("%s %s", &s, &t);
+        int s_len = strlen(s);
+        int t_len = strlen(t);
+
+        int i = 0, j = 0;
+        while (i < s_len && j < t_len)
+        {
+            printf("%c%c", s[i], t[j]);
+            i++;
+            j++;
+        }
+
+        while (i < s_len)
+        {
+            printf("%c", s[i]);
+            i++;
+        }
+
+        while (j < t_len)
+        {
+            printf("%c", t[j]);
+            j++;
+        }
+        printf("\n");
+    }
+
     return 0;
 }
